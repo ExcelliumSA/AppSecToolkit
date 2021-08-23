68d5827767510b6a2005d713a35fead93bb1a204
Dominique RIGHETTO <righettod@users.noreply.github.com>
2021-08-23 08:22:27 +0200

    Add dynamic ENV configuration like for the PS one - #8

--- user_profile.sh
+++ user_profile.sh
@@ -1,1 +1,16 @@
+# Use this file to run your own startup commands
 # See https://github.com/cmderdev/cmder/tree/master/config
+## Add any folder containing EXE file to $PATH
+echo -n "[+] Setting PATH and JAVA_HOME environment variables..."
+cd ..
+javaBinExePath=$(find . -type f -name "java.exe")
+javaBinExePathFolder=$(dirname $(realpath $javaBinExePath))
+javaHome=$(dirname $javaBinExePathFolder)
+export JAVA_HOME=$javaHome
+for p in $(find . -maxdepth 3 -type d)
+do
+	fullPath=$(realpath $p)
+	export PATH=$PATH:$fullPath
+done
+echo "OK"
+alias ll='ls --color=auto -rtl'
